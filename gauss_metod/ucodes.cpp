6b47704b8d7afb86dafece43b35e2bdb31778525
maxwell <m.kurbatov@g.nsu.ru>
2022-09-29 19:14:09 +0700

    gauss method is not working

--- gauss_metod/ucodes.cpp
+++ gauss_metod/ucodes.cpp
@@ -1,5 +1,5 @@
-#include <cstdio>
 #include "ucenv.h"
+#include <cstdio>
 
 extern "C" {
 
@@ -7,16 +7,28 @@ void c_init(int val, OutputDF &df) {
 	df.setValue(val);
 }
 
-void c_sum(OutputDF& out, InputDF& in_a, InputDF& in_b) {
-    out.setValue(in_a.getValue<int>() + in_b.getValue<int>());
+void c_set_k(OutputDF& K, InputDF in_a, InputDF in_b) {
+    K.setValue((int)(in_a.getValue<int>() / in_b.getValue<int>()));
 }
 
-void c_sub(OutputDF& out, InputDF& red, InputDF& sub) {
-    out.setValue(red.getValue<int>() - sub.getValue<int>());
+void c_calculate(OutputDF& K, 
+                 InputDF in_a, 
+                 InputDF in_b,
+                 InputDF in_c) {
+
+//	printf("%d \n", in_a.getValue<int>());
+//
+//	printf("%d \n", in_b.getValue<int>());
+//
+//	printf("%d \n", in_c.getValue<int>());
+//
+//	printf("%d \n", K.getValue<int>());
+//
+    K.setValue((int)(in_a.getValue<int>() - (in_b.getValue<int>() * in_c.getValue<int>())));
 }
 
 void c_iprint(int val) {
-	printf("%d\n", val);
+	printf("%d \n", val);
 }
 
 void c_print(const InputDF &df) {
