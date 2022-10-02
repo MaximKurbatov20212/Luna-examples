8b8e5e3f16870e74552e7e339514d23657d3ada6
maxwell <m.kurbatov@g.nsu.ru>
2022-10-02 20:10:04 +0700

    gauss

--- gauss_metod/ucodes.cpp
+++ gauss_metod/ucodes.cpp
@@ -3,12 +3,16 @@
 
 extern "C" {
 
-void c_init(int val, OutputDF &df) {
+void c_init(double val, OutputDF &df) {
 	df.setValue(val);
 }
 
 void c_set_k(OutputDF& K, InputDF in_a, InputDF in_b) {
-    K.setValue((int)(in_a.getValue<int>() / in_b.getValue<int>()));
+    K.setValue((double)(in_a.getValue<double>() / in_b.getValue<double>()));
+}
+
+void c_set(OutputDF& out, InputDF in) {
+    out.setValue(in.getValue<double>());
 }
 
 void c_calculate(OutputDF& K, 
@@ -16,15 +20,8 @@ void c_calculate(OutputDF& K,
                  InputDF in_b,
                  InputDF in_c) {
 
-//	printf("%d \n", in_a.getValue<int>());
-//
-//	printf("%d \n", in_b.getValue<int>());
-//
-//	printf("%d \n", in_c.getValue<int>());
-//
-//	printf("%d \n", K.getValue<int>());
-//
-    K.setValue((int)(in_a.getValue<int>() - (in_b.getValue<int>() * in_c.getValue<int>())));
+    K.setValue((double)(in_a.getValue<double>() - 
+                (in_b.getValue<double>() * in_c.getValue<double>())));
 }
 
 void c_iprint(int val) {
@@ -32,7 +29,7 @@ void c_iprint(int val) {
 }
 
 void c_print(const InputDF &df) {
-	printf("%d \n", df.getValue<int>());
+	printf("%f \n", df.getValue<double>());
 }
 
 }
