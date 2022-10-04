24c5507e1f9d600dd2f887f296dd8aa74386df95
maxwell <m.kurbatov@g.nsu.ru>
2022-10-05 05:53:51 +0700

    scalar_mul is already completed

--- scalar_mul/ucodes.cpp
+++ scalar_mul/ucodes.cpp
@@ -1,4 +1,5 @@
 #include <cstdio>
+#include <cmath>
 #include "ucenv.h"
 
 extern "C" {
@@ -7,16 +8,29 @@ void c_init(int val, OutputDF &df) {
 	df.setValue(val);
 }
 
-void c_sum(OutputDF& out, InputDF& in_a, InputDF& in_b) {
+void c_get_end(OutputDF& out, InputDF val) {
+    out.setValue(static_cast<int>(log2(val.getValue<int>()) + 1));
+}
+
+void c_set_cur_len(OutputDF& new_len, InputDF old_len) {
+    new_len.setValue(static_cast<int>((old_len.getValue<int>() / 2)
+                                   + (old_len.getValue<int>() % 2)));
+}
+
+void c_sum(OutputDF& out, InputDF in_a, InputDF in_b) {
     out.setValue(in_a.getValue<int>() + in_b.getValue<int>());
 }
 
+void c_mul(OutputDF& out, InputDF in_a, InputDF in_b) {
+    out.setValue(in_a.getValue<int>() * in_b.getValue<int>());
+}
+
 void c_iprint(int val) {
-	printf("%d ", val);
+	printf("%d \n", val);
 }
 
 void c_print(InputDF &df) {
-	printf("%d\n", df.getValue<int>());
+	printf("%d \n", df.getValue<int>());
 }
 
 }
