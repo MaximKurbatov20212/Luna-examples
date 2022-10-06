84b2825e8c4bc12b3e9b306cda64637cee188468
maxwell <m.kurbatov@g.nsu.ru>
2022-10-07 00:20:57 +0700

    fixed example

--- luna_semantic_mistakes_examples/parameters_mismatching/ucodes.cpp
+++ luna_semantic_mistakes_examples/parameters_mismatching/ucodes.cpp
@@ -3,24 +3,7 @@
 
 extern "C" {
 
-void c_init(int val, OutputDF &df) {
-	df.setValue(val);
+void c_foo(InputDF& val, OutputDF &df) {
+	df.setValue(val.getValue<int>());
 }
-
-void c_init_real(double val, OutputDF &df) {
-	df.setValue(val);
-}
-
-void c_sum(OutputDF& out, InputDF& in_a, InputDF& in_b) {
-    out.setValue(in_a.getValue<int>() + in_b.getValue<int>());
-}
-
-void c_iprint(int val) {
-	printf("%d \n", val);
-}
-
-void c_print(InputDF &df) {
-	printf("%d \n", df.getValue<int>());
-}
-
 }
