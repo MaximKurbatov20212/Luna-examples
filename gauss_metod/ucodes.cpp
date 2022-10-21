e93bf1f7c75b99ae009e0fc8b2594db147c3ae8b
maxwell <m.kurbatov@g.nsu.ru>
2022-10-21 21:03:49 +0700

    gauss method is already completed

--- gauss_metod/ucodes.cpp
+++ gauss_metod/ucodes.cpp
@@ -8,33 +8,20 @@ void c_init(double val, OutputDF &df) {
 	df.setValue(val);
 }
 
-void c_init_df(InputDF val, OutputDF &df) {
+void c_init_df(InputDF& val, OutputDF &df) {
 	df.setValue(val.getValue<double>());
 }
 
-void c_set(OutputDF& out, InputDF in) {
-    out.setValue(in.getValue<double>());
-}
-
-void c_iprint(int val) {
-	printf("%d \n", val);
-}
-
-
-void c_print_str(const char* string) {
-	printf("%s\n", string);
-}
-
 void c_print(const InputDF &df) {
-	printf("%f \n", df.getValue<int>());
+	printf("%f \n", df.getValue<double>());
 }
 
-void c_println() {
-	printf("\n");
+void c_i_j_k(int i, int j, int k) {
+    printf("[%d, %d, %d]\n", i, j, k);
 }
 
-void c_i_j_print(int i, int j, InputDF value) {
-    printf("[%d, %d] = %f\n", i, j, value.getValue<double>());
+void c_i_j_print(int i, int j, int gen,  InputDF& value) {
+    printf("gen = %d : [%d, %d] = %f\n", gen, i, j, value.getValue<double>());
 }
 
 }
